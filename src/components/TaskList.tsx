a5b3130cca5e750e5e80384a7848be8296ea59bd
Антон Кобелев <antonkobelev1985@gmail.com>
2024-08-26 17:49:28 +0500

    add component TaskList

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import taskStore from 'src/stores/TaskStore';
+import TaskItem from './TaskItem';
+
+const TaskList: React.FC = () => {
+  return (
+    <div>
+      {taskStore.tasks.map((task) => (
+        <TaskItem key={task.id} task={task} level={0} />
+      ))}
+    </div>
+  );
+};
+
+export default observer(TaskList);
