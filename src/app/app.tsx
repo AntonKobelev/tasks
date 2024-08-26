417be0d69b825cec186e57b5c5a0589571641c67
Антон Кобелев <antonkobelev1985@gmail.com>
2024-08-26 18:24:12 +0500

    add TaskList to App component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TaskForm from '../components/TaskForm';
+import TaskList from '../components/TaskList';
 
 const App: React.FC = () => {
   return (
     <div className="flex p-4 space-x-4">
       <div className="w-1/3">
         <TaskForm />
+        <TaskList />
       </div>
     </div>
   );
